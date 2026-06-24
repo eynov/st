ebe65f7bdb654ea47373a10ea03c815f2f9c57e0
eynov <eynov@pm.me>
2026-06-24 10:34:28 +0800

    更新 config.py

--- Treehole/config.py
+++ Treehole/config.py
@@ -10,6 +10,7 @@ EMAIL_RECEIVER = os.environ.get("EMAIL_RECEIVER", "")
 # Telegram config
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
 ADMIN_USER_ID = os.environ.get("ADMIN_USER_ID", "")
+FORUM_GROUP_ID = int(os.getenv("FORUM_GROUP_ID"))
 # 管理员登录账号密码
 ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "")
